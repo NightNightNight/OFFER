66132b41138c50acca0024c9877df3b778a07095
gaoqi <gq813@foxmail.com>
2018-01-28 19:28:18 +0800

    lcssubstr

---
+++ lcssubstr.py
@@ -0,0 +1,18 @@
+def lcssubstr(s1, s2):   
+    m=[[0 for i in range(len(s2)+1)] for j in range(len(s1)+1)]  #生成0矩阵，为方便后续计算，比字符串长度多了一列  
+    mmax=0   #最长匹配的长度  
+    p=0  #最长匹配对应在s1中的最后一位  
+    for i in range(len(s1)):  
+        for j in range(len(s2)):  
+            if s1[i]==s2[j]:  
+                m[i+1][j+1]=m[i][j]+1  
+                if m[i+1][j+1]>mmax:  
+                    mmax=m[i+1][j+1]  
+                    p=i+1  
+    print('The max length of lcssubstr is ', mmax)
+    print('The max of lcssubstr is ', s1[p-mmax:p]) 
+    
+if __name__ == '__main__':
+    s1 = 'acbdewsdkwkjd'
+    s2 = 'ksjfjsacbdewk'
+    lcssubstr(s1, s2)
