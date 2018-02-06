e48c73f6efe3aae49260d4d2219cbe6b728680bd
gaoqi <gq813@foxmail.com>
2018-02-06 12:55:59 +0800

    add heap_sort

--- main.cpp
+++ main.cpp
@@ -19,7 +19,6 @@ void quick_sort(int list[], int left, int right){
                              list[right] = list[left];
                   }
                   list[left] = key;
-     
                   quick_sort(list, low, left-1);
                   quick_sort(list, left+1, high);
      }
@@ -72,12 +71,43 @@ int binary_search(int list[], int length, int key){
 }
 
 
+//构造最大堆
+void MaxHeapFixDown(int a[], int i, int n){
+    int j = 2*i+1;
+    int temp = a[i];
+    while(j<n){
+        if(j+1<n&&a[j]<a[j+1])
+            ++j;
+        if(temp>a[j])
+            break;
+        else{
+            a[i]=a[j];
+            i=j;
+            j=2*i+1;
+        }
+    }
+    a[i]=temp;
+}
 
+void swap(int &a, int &b){
+     a = a + b;
+     b = a - b;
+     a = a - b;
+}
+
+//堆排序
+void HeapSort(int a[], int n){
+    for(int i= n/2-1;i>=0;i--)
+        MaxHeapFixDown(a,i,n);
+    for(int i=n-1;i>=1;i--){
+        swap(a[i],a[0]);
+        MaxHeapFixDown(a,0,i);
+    }
+}
 
 
 int main(int argc, char *argv[])
 {
-    
     int list[8] = {4, 7, 8, 2, 9, 1, 3, 5};
     int length = sizeof(list) / sizeof(int);
     
@@ -87,15 +117,15 @@ int main(int argc, char *argv[])
     cout<<endl;
     
     //quick_sort(list, 0, length-1);
-    merge_sort(list, 0, length - 1);
-    //MergeSort(list, 0, length - 1);
+    //merge_sort(list, 0, length - 1);
+    HeapSort(list, length);
     
     for(int i = 0; i < length; i++)
             cout << list[i];
     cout<<endl;
     
-    int pos = binary_search(list, length, 3);
-    cout << pos << endl;
+//    int pos = binary_search(list, length, 3);
+//    cout << pos << endl;
     
     cout << "Hello World";
     system("PAUSE");
